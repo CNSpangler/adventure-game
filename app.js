e93f562d5e12521462a2495dc887aecf98b286b3
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-30 09:06:51 -0800

    technical plan in comments

---
+++ app.js
@@ -0,0 +1,11 @@
+// Header with game name
+// Instructions/overview of game
+// Form to collect user name
+// STRETCH: Choose your axe
+// Submit button with event listener to:
+    // Save user name to local storage (make and save user object)
+        // name (from form data)
+        // set power-chords state to 0
+        // set hair-bigness to 3
+    // Send user to guitar-map page
+    // STRETCH: guitar riff on submit
---
+++ guitar-map/guitar-map.js
@@ -0,0 +1,9 @@
+// list of quests = guitar picks
+    // turn each into a query param link
+    // generate new page for specific quest
+
+// for each quest page
+    // show header with user stats
+    // pull encounter scenario from metadata
+    // pull choices from metadata
+        // each choice a radio button
