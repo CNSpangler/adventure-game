f63e1ecfe7421fe0d3c2bfdae43e55febe96a333
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-29 17:10:01 -0800

    set up file structure

---
+++ data/encounters.js
@@ -0,0 +1,20 @@
+// Scorpion appears and declares “Here I am.” How do you respond?
+// •	“Rock you like a hurricane.”
+// •	“Who are you? Please explain.”
+// •	“You seem like you are insane.”
+// Oh no, you rocked too hard and a guitar string went flying. Bon Jovi has been shot through the heart, and you’re to blame!
+// •	Tell him about the blockchain.
+// •	You think that is a real shame.
+// •	You give love a bad name.
+// A Def Leppard approaches, speaking in riddles: “I’m hot, sticky sweet, from my head to my feet.” What is your answer?
+// •	“Yes, you do seem to be.”
+// •	“Pour some sugar on me.”
+// •	“Now you’re covered in bees.”
+// Whoa, we’re halfway there! Whoa:
+// •	Oh shit, it’s a bear!
+// •	I take my steak rare!
+// •	Livin’ on a prayer!
+// Oh no, a Motley Crue bursts out of the forest! Their leader tells you “Handful of grease in my hair feels right, but what I need to get me tight are those…” What do you offer them?
+// •	Squirrels, squirrels, squirrels
+// •	Pearls, pearls, pearls
+// •	Girls, girls, girls
