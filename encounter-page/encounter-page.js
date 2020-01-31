6a98aaa216ddd251501eca44cf67253f886ef94e
Chelsea Spangler <chelseanspangler@gmail.com>
2020-01-30 16:10:24 -0800

    add pseudocode for all pages

---
+++ encounter-page/encounter-page.js
@@ -0,0 +1,23 @@
+// Hard code a section/div/form/button (maybe a fieldset instead of section? make use of legend?)
+// Hard code a buttion inside the form
+// get the data
+// get the data for the correct question using queryParam and findById
+    // put the encounter description in the div
+// For each choice: dynamically render a radio button
+    // make a label
+    // Make a span
+    // use data to set the span's text content to encounter.choice.id
+    // make an input of type radio, name "encounter"
+    // Append the span to the label
+// for the form, add an event listener on submit
+    // get formData (new FormData)
+    // call the encounter completed in state; redirect to the map
+    // use the user selection in the formData to update the DOM and change state
+        // set textContent of...what element?
+        // increment hp
+        // update hp in localStorage (or does this automatically happen?)
+        // increment pc
+        // update pc in localStorage (or does this automatically happen?)
+
+
+
---
+++ results/results.js
@@ -0,0 +1,5 @@
+// get user choices from localStorage
+// total up hp
+// total up pc
+// get some DOM and update to show the appropriate results scenario
+// clear local storage for this user
