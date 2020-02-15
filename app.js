a5facfbb9cd4a03fbe18e5b98c0f14c996577b99
Chelsea Spangler <chelseanspangler@gmail.com>
2020-02-15 10:12:29 -0800

    fixed sound files

--- app.js
+++ app.js
@@ -14,4 +14,6 @@ form.addEventListener('submit', (event) => {
     saveBand(band);
 
     window.location = 'guitar-map/index.html';
+    
+    alert(`Don't fret! You'll find our rock idols soon. Click on any of the guitar picks to choose your first adventure. Keep an eye on your stats in the top right corner. If you run out of HP (Hair Points), you lose. Collect as many PC (Power Chords) as you can! Good luck!`);
 });
